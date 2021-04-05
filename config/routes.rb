bade4ab7d6c99773de8ed891d6d2c5d4d937267b
okunoyuto0000 <okunoyuto0000@gmail.com>
2021-04-05 13:16:47 +0000

    [Finish] 課題３

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:index, :show, :edit, :update]
 end
